ee648ee42fa7f8c8981eb6a1cbabe03b9a1ab048
Rachana DS <r.dhalli.srinivasa@accenture.com>
2019-02-22 11:02:00 +0530

    Commiting initial files.

---
+++ 5/src/index.js
@@ -0,0 +1,28 @@
+import { cube } from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
+function component() {
+    var element = document.createElement('pre');
+
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+
+    return element;
+  }
+  
+  let element = component(); // Store the element to re-render on print.js changes
+  document.body.appendChild(element);
+  
+  if (module.hot) {
+      module.hot.accept('./print.js', function() {
+          console.log('Accepting the updated printMe module!');
+          document.body.removeChild(element);
+          element = component(); // Re-render the "component" to update the click handler
+          document.body.appendChild(element);
+        })
+    }
